3547d84e215df13a4c93b66224550a4e4350baa1
vvhh3 <vvhh3144@gmail.com>
2026-05-12 18:22:20 +0400

    theme

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from './Components/theme/ThemeProvider'
 
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { theme, toggleTheme } = useTheme()
 
   return (
-    <>
-      
-    </>
+    <div className="">
+      <div className="mx-auto flex items-center py-4 px-4">
+        <button
+          onClick={toggleTheme}
+          className="rounded-full flex justify-center items-center
+        text-black cursor-pointer transition-colors duration-300 hover:opacity-90  dark:text-black"
+        >
+          {theme === 'light' ? <Moon /> : <Sun />}
+        </button>
+      </div>
+
+    </div>
   )
 }
 
