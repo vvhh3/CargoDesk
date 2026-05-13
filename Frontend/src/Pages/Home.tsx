49de0932d6df6a2f455a48679151137ea710e561
vvhh3 <vvhh3144@gmail.com>
2026-05-13 18:58:07 +0400

    add card

--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -47,53 +47,84 @@ const Home = () => {
                 </div>
 
                 {/* DashBoard demo */}
-                <div className="w-full max-w-6xl mx-auto">
-                    <div className="rounded-3xl bg-white/5 shadow-cyan-500/50">
-                        <div className="flex flex-col w-full px-10 pt-8 pb-10">
+                <div className="w-full bg-white/5 rounded-3xl max-w-6xl mx-auto shadow-cyan-700/50 shadow-2xl">
+                    {/* <div className=""> */}
+                    <div className="flex flex-col w-full px-10 pt-8 pb-10">
 
-                            <div className="flex gap-2 max-h-3">
-                                <div className="w-3 h-3 bg-red-500 rounded-full"></div>
-                                <div className="w-3 h-3 bg-yellow-600 rounded-full"></div>
-                                <div className="w-3 h-3 bg-green-600 rounded-full"></div>
-                            </div>
+                        <div className="flex gap-2 max-h-3">
+                            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+                            <div className="w-3 h-3 bg-yellow-600 rounded-full"></div>
+                            <div className="w-3 h-3 bg-green-600 rounded-full"></div>
+                        </div>
 
-                            <div className="flex mt-7 gap-5 w-full">
+                        <div className="flex mt-7 gap-5 w-full">
 
-                                <div>
-                                    <div className="w-70 border border-white/10 rounded-2xl p-5 h-[450px]">
-                                        <div className="space-y-3">
-                                            {[1, 2, 3, 4, 5, 6].map(i => (
-                                                <div className="h-12 rounded-xl bg-white/5 backdrop-blur-sm" key={i}></div>
-                                            ))}
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                                <div className="flex flex-1 flex-col min-w-0">
-
-                                    <div className="grid grid-cols-3 gap-4 w-full">
-                                        {['1', '2', '3'].map(i => (
-                                            <div key={i} className="p-4 rounded-2xl  border border-white/10 backdrop-blur-xl">
-                                                <div className="h-3 w-16 bg-white/20 rounded mb-3"></div>
-                                                <div className="h-6 w-24 bg-white/30 rounded"></div>
-                                            </div>
+                            <div>
+                                <div className="w-70 border border-white/10 rounded-2xl p-5 h-[450px]">
+                                    <div className="space-y-3">
+                                        {[1, 2, 3, 4, 5, 6].map(i => (
+                                            <div className="h-12 rounded-xl bg-white/5 backdrop-blur-sm" key={i}></div>
                                         ))}
                                     </div>
 
-                                    <div className="h-[350px] w-full rounded-2xl border border-white/10 backdrop-blur-xl p-6 mt-5">
-                                        <div className="flex items-end justify-between h-full">
-                                            {[40, 70, 55, 85, 60 , 68, 90, 75, 95].map((height, i) => (
-                                                <div key={i} className="flex-1 max-w-12 rounded-t-xl bg-[#7C3AED] " style={{ height: `${height}%` }}></div>
-                                            ))}
+                                </div>
+                            </div>
+
+                            <div className="flex flex-1 flex-col min-w-0">
+
+                                <div className="grid grid-cols-3 gap-4 w-full">
+                                    {['1', '2', '3'].map(i => (
+                                        <div key={i} className="p-4 rounded-2xl  border border-white/10 backdrop-blur-xl">
+                                            <div className="h-3 w-16 bg-white/20 rounded mb-3"></div>
+                                            <div className="h-6 w-24 bg-white/30 rounded"></div>
                                         </div>
-                                    </div>
+                                    ))}
                                 </div>
 
+                                <div className="h-[350px] w-full rounded-2xl border border-white/10 backdrop-blur-xl p-6 mt-5">
+                                    <div className="flex items-end justify-between h-full">
+                                        {[40, 70, 55, 85, 60, 68, 90, 75, 95].map((height, i) => (
+                                            <div key={i} className="flex-1 max-w-12 rounded-t-xl bg-[#7C3AED] " style={{ height: `${height}%` }}></div>
+                                        ))}
+                                    </div>
+                                </div>
                             </div>
+
                         </div>
                     </div>
+                    {/* </div> */}
                 </div>
+            </section>
+
+            <section>
+
+                    <div className="flex justify-center items-center gap-5 mt-20 mb-10">
+                        <div className="flex flex-col border border-[#8B5CF6]/5 rounded-2xl px-10 py-5
+                        shadow-[#8B5CF6] shadow-2xl bg-white/5 ">
+                            <span className="text-[#8B5CF6] text-2xl">50K+</span>
+                            <span className="text-zinc-400">Active Shipments</span>
+                        </div>
+
+                        <div className="flex flex-col border border-[#2563EB]/5 rounded-2xl px-10 py-5
+                        shadow-[#2563EB] shadow-2xl bg-white/5 ">
+                            <span className="text-[#2563EB] text-2xl">99%</span>
+                            <span className="text-zinc-400">Uptime SLA</span>
+                        </div>
+
+                        <div className="flex flex-col border border-[#16A34A]/5 rounded-2xl px-10 py-5
+                        shadow-[#16A34A] shadow-2xl bg-white/5 ">
+                            <span className="text-[#16A34A] text-2xl">~2min</span>
+                            <span className="text-zinc-400">Avg Response</span>
+                        </div>
+
+                        <div className="flex flex-col border border-[#D97706]/5 rounded-2xl px-10 py-5
+                        shadow-[#D97706] shadow-2xl bg-white/5 ">
+                            <span className="text-[#D97706] text-2xl">100+</span>
+                            <span className="text-zinc-400">Countries</span>
+                        </div>
+
+                    </div>
+
 
             </section>
         </div>
