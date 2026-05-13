73af1b80a0b0990d714a7190c550be84a5fa4c4e
vvhh3 <vvhh3144@gmail.com>
2026-05-13 18:09:58 +0400

    end dashboard

--- Frontend/src/Components/Home/Header.tsx
+++ Frontend/src/Components/Home/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="flex justify-between items-center px-30 py-7 bg-black border-b border-white/10">
             <div className="flex gap-3 items-center">
-                <div className="flex justify-center items-center rounded-xl w-10 h-10 bg-gradient-to-br from-[#7C3AED] to-[#3B82F6]">
+                <div className="flex justify-center items-center rounded-xl w-10 h-10 bg-linear-to-br from-[#7C3AED] to-[#3B82F6]">
                     <Link to="/"><Package className="text-2xl text-white " /> </Link>
                 </div>
                 <h1 className="text-white text-2xl">CargoDesk</h1>
@@ -16,7 +16,7 @@ const Header = () => {
                 <a href="#" className="hover:text-white duration-500">Workflow</a>
                 <a href="#" className="hover:text-white duration-500">Pricing</a>
                 <Link to="" className="hover:text-white duration-500">Sign In</Link>
-                <button className="w-28 h-13 text-white rounded-2xl bg-gradient-to-br from-[#7C3AED] to-[#3B82F6] duration-300 hover:scale-102 cursor-pointer">
+                <button className="w-28 h-13 text-white rounded-2xl bg-linear-to-br from-[#7C3AED] to-[#3B82F6] duration-300 hover:scale-102 cursor-pointer">
                     Get Started
                 </button>
             </div>
--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -14,6 +14,7 @@ const Home = () => {
                 <Header />
             </nav>
 
+            {/* Header-main */}
             <section className="flex flex-col items-center">
 
                 <div className="absolute top-10 left-1/4 bg-[#7C3AED]/20 w-96 h-96 rounded-full blur-[120px]"></div>
@@ -24,7 +25,7 @@ const Home = () => {
                     <span className="text-sm text-zinc-300">Trusted by 10,000+ businesses worldwide</span>
                 </div>
 
-                <div className="mt-4">
+                <div className="mt-4 mb-25">
                     <div className="flex items-center flex-col text-7xl gap-0">
                         <span className="text-white">Transform Your</span>
                         <span className="text-[#7C3AED]">Cargo Operations</span>
@@ -35,13 +36,62 @@ const Home = () => {
                     </div>
 
                     <div className="flex justify-center gap-5">
-                        <Link to='/registry' className="flex justify-center items-center gap-2 text-white px-8 py-4 rounded-2xl bg-gradient-to-r from-[#7C3AED] to-[#8B5CF6] text-lg hover:from-[#8B5CF6] hover:to-[#7C3AED]">
+                        <Link to='/registry' className="flex justify-center items-center gap-2 text-white px-8 py-4 rounded-2xl bg-linear-to-r from-[#7C3AED] to-[#8B5CF6] text-lg hover:from-[#8B5CF6] hover:to-[#7C3AED]">
                             Start Free Trial <MoveRight />
                         </Link>
                         <button className="flex justify-center items-center gap-2 
                         text-white px-8 py-4 rounded-2xl bg-white/5 cursor-pointer border border-white/10 hover:bg-white/10 duration-500">
                             Watch Demo
                         </button>
+                    </div>
+                </div>
+
+                {/* DashBoard demo */}
+                <div className="w-full max-w-6xl mx-auto">
+                    <div className="rounded-3xl bg-white/5 shadow-cyan-500/50">
+                        <div className="flex flex-col w-full px-10 pt-8 pb-10">
+
+                            <div className="flex gap-2 max-h-3">
+                                <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+                                <div className="w-3 h-3 bg-yellow-600 rounded-full"></div>
+                                <div className="w-3 h-3 bg-green-600 rounded-full"></div>
+                            </div>
+
+                            <div className="flex mt-7 gap-5 w-full">
+
+                                <div>
+                                    <div className="w-70 border border-white/10 rounded-2xl p-5 h-[450px]">
+                                        <div className="space-y-3">
+                                            {[1, 2, 3, 4, 5, 6].map(i => (
+                                                <div className="h-12 rounded-xl bg-white/5 backdrop-blur-sm" key={i}></div>
+                                            ))}
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-1 flex-col min-w-0">
+
+                                    <div className="grid grid-cols-3 gap-4 w-full">
+                                        {['1', '2', '3'].map(i => (
+                                            <div key={i} className="p-4 rounded-2xl  border border-white/10 backdrop-blur-xl">
+                                                <div className="h-3 w-16 bg-white/20 rounded mb-3"></div>
+                                                <div className="h-6 w-24 bg-white/30 rounded"></div>
+                                            </div>
+                                        ))}
+                                    </div>
+
+                                    <div className="h-[350px] w-full rounded-2xl border border-white/10 backdrop-blur-xl p-6 mt-5">
+                                        <div className="flex items-end justify-between h-full">
+                                            {[40, 70, 55, 85, 60 , 68, 90, 75, 95].map((height, i) => (
+                                                <div key={i} className="flex-1 max-w-12 rounded-t-xl bg-[#7C3AED] " style={{ height: `${height}%` }}></div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
